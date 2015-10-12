969befffc9e9e3373d76465f45ed58cc0c722aef
tedye <tedye@bu.edu>
2015-10-12 10:49:47 -0700

    lab3

---
+++ Lab3/default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="default.aspx.cs" Inherits="_default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Find Anagrams</title>
+    <style type="text/css"></style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="background-color: #C0C0C0; border-style: solid; border-width: 1px; margin: auto; width: 800px; padding: 10px" >
+        <h1 style="text-align: center">Find Anagrams</h1>
+        <br />
+        Enter a character string:&nbsp;
+        <asp:TextBox ID="InputString" runat="server" Width="147px"></asp:TextBox>
+        &nbsp;&nbsp;
+        <asp:CheckBox ID="checkbox" runat="server" />
+        Eliminate Duplicates
+        <br />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" style="height:85px;width:150px;" size="4"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Button ID="FindAnagram" runat="server" Text="Find Anagrams" OnClick="FindAnagram_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Comment" runat="server" Text=""></asp:Label>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Lab3/default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _default : System.Web.UI.Page
+{
+    private HashSet<string> dupCheck;
+    private bool dupcheck;
+    private int cnt;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        dupCheck = new HashSet<string>();
+    }
+
+    protected void FindAnagram_Click(object sender, EventArgs e)
+    {
+        string anagram = InputString.Text;
+        cnt = 0;
+        dupCheck.Clear();
+        dupcheck = checkbox.Checked;
+
+        // clean up 
+        InputString.Text = "";
+        ListBox1.Items.Clear();
+        Comment.Text = "";
+
+        if (anagram.Length > 8 || anagram.Length < 1)
+        {
+            Comment.Text = "Please enter a string from 1 to 8 characters";
+            return;
+        }
+
+        my_permute(anagram.ToCharArray(), 0, anagram.Length - 1);
+
+        if (dupcheck){
+            cnt = dupCheck.Count();
+        }
+
+        Comment.Text = cnt.ToString() + " anagrams found.";
+    }
+
+    protected void my_permute(char[] anagram, int start, int end )
+    {
+        int j;
+        string res;
+        if (start == end)
+        {
+            res = new string(anagram);
+            if (dupcheck)
+            {
+                if (!dupCheck.Contains(res))
+                {
+                    dupCheck.Add(res);
+                    ListBox1.Items.Add(res);
+                }
+            }
+            else
+            {
+                ListBox1.Items.Add(res);
+                cnt++;
+            }
+            return;
+            
+        }
+        else
+        {
+            for (j = start; j <= end; j++)
+            {
+                swap(anagram,start, j);
+                my_permute(anagram, start + 1, end);
+                swap(anagram,start, j);
+            }
+        }
+    }
+
+    protected void swap(char [] a, int i, int j)
+    {
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
